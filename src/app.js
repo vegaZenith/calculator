41d5ba802fcd0308bc77c5432b2855182c9ddcd4
Dan Lehman <5494894+vegaZenith@users.noreply.github.com>
2024-09-12 13:28:24 -0400

    add operate function Added a function that takes in 2 numbers and an operator and completes the desired operation.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function operate(a, b, op){
+    switch(op){
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+        default:
+            return "error";
+    }
+}
+
 function add(a,b){
     return a + b;
 }
