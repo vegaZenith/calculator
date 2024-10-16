b55861e7ac912e719f2f2cd7589184e8a220af30
Dan Lehman <5494894+vegaZenith@users.noreply.github.com>
2024-10-16 09:42:28 -0400

    fix bug where equals button kept old values Fixed a bug where pressing the equals button did not clear data from previous calculations causing unintended results.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,63 @@
-const buttons = document.querySelectorAll(".number-button");
-buttons.forEach((button) => {
+let number1 = null, number2 = null, operator = null;
+let operatorClicked = false;
+
+const numbers = document.querySelectorAll(".number-button");
+numbers.forEach((button) => {
     button.addEventListener("click", updateDisplay);
 });
+
+const operators = document.querySelectorAll(".operator");
+operators.forEach((operator) => {
+    operator.addEventListener("click", operatorClick)
+});
+
+const equals = document.querySelector(".equals");
+equals.addEventListener("click", equalsClick);
+
+function clearClick(event){
+    const display = document.querySelector(".display");
+    display.textConent = "";
+    number1 = null;
+    number2 = null;
+
+}
 
 function updateDisplay(event){
     const text = event.target.textContent;
     const display = document.querySelector(".display");
-    display.textContent += text;
+    
+    if(!operatorClicked){
+        display.textContent += text;      
+    } else {
+        display.textContent = text;
+        operatorClicked = false;
+    }
+}
+
+function equalsClick(event){
+    if(number1 === null || operator === null){
+        return;
+    }
+
+    const display = document.querySelector(".display");
+    number2 = display.textContent;
+    display.textContent = operate(Number(number1), Number(number2), operator);
+    number1 = null;
+    number2 = null;
+}
+
+function operatorClick(event){
+    operatorClicked = true;
+    const display = document.querySelector(".display");
+    if(number1 === null){
+        number1 = display.textContent;
+        operator = event.target.textContent;
+    } else {
+        number2 = display.textContent;
+        number1 = operate(Number(number1), Number(number2), operator);
+        display.textContent = number1;
+        operator = event.target.textContent;
+    }
 }
 
 function operate(a, b, op){
